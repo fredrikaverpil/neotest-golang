b47f78b8ab7a96f7e3223f38c3b93940cb180dbc
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-05-11 19:29:11 +0200

    feat: run tests with busted and neovim

---
+++ test/unit/is_test_file_spec.lua
@@ -0,0 +1,18 @@
+local adapter = require("neotest-golang")
+
+describe("Is test file", function()
+  it("True - Path to file", function()
+    local file_path = "foo/bar/baz_test.go"
+    assert.is_true(adapter.is_test_file(file_path))
+  end)
+
+  it("True - Just filename", function()
+    local file_path = "foo_test.go"
+    assert.is_true(adapter.is_test_file(file_path))
+  end)
+
+  it("False - Not a test file", function()
+    local file_path = "foo_bar.go"
+    assert.is_false(adapter.is_test_file(file_path))
+  end)
+end)
