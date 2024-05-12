88fa0236faf8b49131a714caafb572c255ced402
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-05-12 13:03:21 +0200

    refactor: rename files

--- test/go/main.go
+++ test/go/main.go
@@ -2,10 +2,7 @@ package main
 
 import "fmt"
 
+// Dummy main function.
 func main() {
 	fmt.Println("Hello, World!")
-}
-
-func Add(a, b int) int {
-	return a + b
 }
--- test/go/positions_test.go
+++ test/go/positions_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-// A dummy test, just to assert that Go tests can run
+// A dummy test, just to assert that Go tests can run.
 func TestAdd(t *testing.T) {
 	if Add(1, 2) != 3 {
 		t.Fail()
