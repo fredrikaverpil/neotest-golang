ea308337455247daa9aac501a3f4e604f6e25de2
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-05-12 13:11:03 +0200

    fix: clean up installer script

--- tests/install.sh
+++ tests/install.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 set -e
 
-# This script is executed via tests/minimal_init.lua
-
-GITHUB="https://github.com"
-GITHUB_PLENARY="$GITHUB/nvim-lua/plenary.nvim"
-GITHUB_TELESCOPE="$GITHUB/nvim-telescope/telescope.nvim"
-GITHUB_LSPCONFIG="$GITHUB/neovim/nvim-lspconfig"
-GITHUB_NIO="$GITHUB/nvim-neotest/nvim-nio"
-GITHUB_NEOTEST="$GITHUB/nvim-neotest/neotest"
-GITHUB_TREESITTER="$GITHUB/nvim-treesitter/nvim-treesitter"
-
-SCRIPT_FILE=$(readlink -f "${BASH_SOURCE[0]}")
-REPO_DIR=$(dirname "$(dirname "$SCRIPT_FILE")")
-
+# This script is executed by tests/minimal_init.lua
+REPOS=(
+	"https://github.com/nvim-lua/plenary.nvim"
+	"https://github.com/nvim-telescope/telescope.nvim"
+	"https://github.com/neovim/nvim-lspconfig"
+	"https://github.com/nvim-neotest/nvim-nio"
+	"https://github.com/nvim-neotest/neotest"
+	"https://github.com/nvim-treesitter/nvim-treesitter"
+)
+THIS_SCRIPT_FILE=$(readlink -f "${BASH_SOURCE[0]}")
+REPO_DIR=$(dirname "$(dirname "$THIS_SCRIPT_FILE")")
 TEST_ALL_DIR="$REPO_DIR/.tests/all/site/pack/deps/start"
 
 clone() {
@@ -26,9 +24,6 @@ clone() {
 
 # Just for the main minimal_init.lua for neotest
 mkdir -p $TEST_ALL_DIR
-clone $GITHUB_PLENARY "$TEST_ALL_DIR/plenary.nvim"
-clone $GITHUB_LSPCONFIG "$TEST_ALL_DIR/lspconfig.nvim"
-clone $GITHUB_TELESCOPE "$TEST_ALL_DIR/telescope.nvim"
-clone $GITHUB_NIO "$TEST_ALL_DIR/nvim-nio"
-clone $GITHUB_NEOTEST "$TEST_ALL_DIR/neotest"
-clone $GITHUB_TREESITTER "$TEST_ALL_DIR/nvim-treesitter"
+for repo in "${REPOS[@]}"; do
+	clone "$repo" "$TEST_ALL_DIR/$(basename $repo)"
+done
