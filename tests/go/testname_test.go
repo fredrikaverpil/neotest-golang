c0a34a59b7f165a576c5657c5e8f37e19dc5f936
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-05-22 19:24:01 +0200

    test: test name conversion

---
+++ tests/go/testname_test.go
@@ -0,0 +1,18 @@
+package main
+
+import "testing"
+
+// A dummy test, just to assert that Go tests can run.
+func TestNames(t *testing.T) {
+	t.Run("Mixed case with space", func(t *testing.T) {
+		if Add(1, 2) != 3 {
+			t.Fail()
+		}
+	})
+
+	t.Run("Comma , and ' are ok to use", func(t *testing.T) {
+		if Add(1, 2) != 3 {
+			t.Fail()
+		}
+	})
+}
