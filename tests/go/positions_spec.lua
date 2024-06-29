8aedf0b423f91b8ea443d12d750fd20a6b696cd2
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-06-29 12:58:22 +0200

    test: expand tests on top-level, sub-test

--- tests/go/positions_spec.lua
+++ tests/go/positions_spec.lua
@@ -10,16 +10,100 @@ describe("Discovery of test positions", function()
         id = test_filepath,
         name = vim.fn.fnamemodify(test_filepath, ":t"),
         path = test_filepath,
-        range = { 0, 0, 10, 0 },
+        range = { 0, 0, 59, 0 }, -- NOTE: this always gets changed when tests are added or removed
         type = "file",
       },
       {
         {
-          id = test_filepath .. "::TestAdd",
-          name = "TestAdd",
+          id = test_filepath .. "::TestTopLevel",
+          name = "TestTopLevel",
           path = test_filepath,
-          range = { 5, 0, 9, 1 },
+          range = { 4, 0, 8, 1 },
           type = "test",
+        },
+      },
+      {
+        {
+          id = test_filepath .. "::TestTopLevelWithSubTest",
+          name = "TestTopLevelWithSubTest",
+          path = test_filepath,
+          range = { 10, 0, 16, 1 },
+          type = "test",
+        },
+        {
+          {
+            id = test_filepath .. '::TestTopLevelWithSubTest::"SubTest"',
+            name = '"SubTest"',
+            path = test_filepath,
+            range = { 11, 1, 15, 3 },
+            type = "test",
+          },
+        },
+      },
+      {
+        {
+          id = test_filepath .. "::TestTopLevelWithTableTests",
+          name = "TestTopLevelWithTableTests",
+          path = test_filepath,
+          range = { 18, 0, 36, 1 },
+          type = "test",
+        },
+        {
+          {
+            id = test_filepath .. '::TestTopLevelWithTableTests::"TableTest1"',
+            name = '"TableTest1"',
+            path = test_filepath,
+            range = { 25, 2, 25, 47 },
+            type = "test",
+          },
+        },
+        {
+          {
+            id = test_filepath .. '::TestTopLevelWithTableTests::"TableTest2"',
+            name = '"TableTest2"',
+            path = test_filepath,
+            range = { 26, 2, 26, 47 },
+            type = "test",
+          },
+        },
+      },
+      {
+        {
+          id = test_filepath .. "::TestTopLevelWithSubTestWithTableTests",
+          name = "TestTopLevelWithSubTestWithTableTests",
+          path = test_filepath,
+          range = { 38, 0, 58, 1 },
+          type = "test",
+        },
+        {
+          {
+            id = test_filepath
+              .. '::TestTopLevelWithSubTestWithTableTests::"SubTest"',
+            name = '"SubTest"',
+            path = test_filepath,
+            range = { 39, 1, 57, 3 },
+            type = "test",
+          },
+          {
+            {
+              id = test_filepath
+                .. '::TestTopLevelWithSubTestWithTableTests::"SubTest"::"TableTest1"',
+              name = '"TableTest1"',
+              path = test_filepath,
+              range = { 46, 3, 46, 48 },
+              type = "test",
+            },
+          },
+          {
+            {
+              id = test_filepath
+                .. '::TestTopLevelWithSubTestWithTableTests::"SubTest"::"TableTest2"',
+              name = '"TableTest2"',
+              path = test_filepath,
+              range = { 47, 3, 47, 48 },
+              type = "test",
+            },
+          },
         },
       },
     }
@@ -31,6 +115,6 @@ describe("Discovery of test positions", function()
 
     -- Assert
     local result = tree:to_list()
-    assert.are.same(expected, result)
+    assert.are.same(vim.inspect(expected), vim.inspect(result))
   end)
 end)
