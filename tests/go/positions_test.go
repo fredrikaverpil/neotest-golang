5dbd763ceb3f9e1be63b83a83dd93e66b5db778b
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-06-29 13:17:43 +0200

    test: expand tests (top-level, subtests, table tests) (#66)

--- tests/go/positions_test.go
+++ tests/go/positions_test.go
@@ -2,9 +2,58 @@ package main
 
 import "testing"
 
-// A dummy test, just to assert that Go tests can run.
-func TestAdd(t *testing.T) {
+func TestTopLevel(t *testing.T) {
 	if Add(1, 2) != 3 {
 		t.Fail()
 	}
+}
+
+func TestTopLevelWithSubTest(t *testing.T) {
+	t.Run("SubTest", func(t *testing.T) {
+		if Add(1, 2) != 3 {
+			t.Fail()
+		}
+	})
+}
+
+func TestTopLevelWithTableTests(t *testing.T) {
+	tt := []struct {
+		name     string
+		x        int
+		y        int
+		expected int
+	}{
+		{name: "TableTest1", x: 1, y: 2, expected: 3},
+		{name: "TableTest2", x: 3, y: 4, expected: 7},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			if Add(tc.x, tc.y) != tc.expected {
+				t.Fail()
+			}
+		})
+	}
+}
+
+func TestTopLevelWithSubTestWithTableTests(t *testing.T) {
+	t.Run("SubTest", func(t *testing.T) {
+		tt := []struct {
+			name     string
+			x        int
+			y        int
+			expected int
+		}{
+			{name: "TableTest1", x: 1, y: 2, expected: 3},
+			{name: "TableTest2", x: 3, y: 4, expected: 7},
+		}
+
+		for _, tc := range tt {
+			t.Run(tc.name, func(t *testing.T) {
+				if Add(tc.x, tc.y) != tc.expected {
+					t.Fail()
+				}
+			})
+		}
+	})
 }
