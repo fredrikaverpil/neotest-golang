92a0f4d26e9cf6a47aa6f3dce0b685bb77a93b4c
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-07-11 19:06:28 +0200

    test: testify suites

---
+++ tests/go/testify/positions_spec.lua
@@ -0,0 +1,172 @@
+local nio = require("nio")
+local _ = require("plenary")
+
+local adapter = require("neotest-golang")
+local options = require("neotest-golang.options")
+local testify = require("neotest-golang.features.testify")
+
+local function compareIgnoringKeys(t1, t2, ignoreKeys)
+  local function copyTable(t, ignoreKeys)
+    local copy = {}
+    for k, v in pairs(t) do
+      if not ignoreKeys[k] then
+        if type(v) == "table" then
+          copy[k] = copyTable(v, ignoreKeys)
+        else
+          copy[k] = v
+        end
+      end
+    end
+    return copy
+  end
+  return copyTable(t1, ignoreKeys), copyTable(t2, ignoreKeys)
+end
+
+describe("With testify_enabled=false", function()
+  it("Discover test functions", function()
+    -- Arrange
+    local test_filepath = vim.loop.cwd()
+      .. "/tests/go/testify/positions_test.go"
+    local expected = {
+      {
+        id = test_filepath,
+        name = "positions_test.go",
+        path = test_filepath,
+        type = "file",
+      },
+      {
+        {
+          id = test_filepath .. "::TestExampleTestSuite",
+          name = "TestExampleTestSuite",
+          path = test_filepath,
+          type = "test",
+        },
+      },
+      {
+        {
+          id = test_filepath .. "::TestTrivial",
+          name = "TestTrivial",
+          path = test_filepath,
+          type = "test",
+        },
+      },
+    }
+
+    -- Act
+    ---@type neotest.Tree
+    local tree =
+      nio.tests.with_async_context(adapter.discover_positions, test_filepath)
+
+    -- Assert
+    local result = tree:to_list()
+    local ignoreKeys = { range = true }
+    local expectedCopy, resultCopy =
+      compareIgnoringKeys(expected, result, ignoreKeys)
+    assert.are.same(vim.inspect(expectedCopy), vim.inspect(resultCopy))
+    assert.are.same(expectedCopy, resultCopy)
+  end)
+end)
+
+describe("With testify_enabled=true", function()
+  it("Discover namespaces, test methods and test function", function()
+    -- Arrange
+    local test_filepath = vim.loop.cwd()
+      .. "/tests/go/testify/positions_test.go"
+    options.set({ testify = true }) -- enable testify
+    testify.lookup.generate() -- generate lookup
+
+    local expected = {
+      {
+        id = test_filepath,
+        name = "positions_test.go",
+        path = test_filepath,
+        type = "file",
+      },
+      {
+        {
+          id = test_filepath .. "::TestExampleTestSuite",
+          name = "TestExampleTestSuite",
+          path = test_filepath,
+          type = "namespace",
+        },
+        {
+          {
+            id = test_filepath .. "::TestExampleTestSuite::TestExample",
+            name = "TestExample",
+            path = test_filepath,
+            type = "test",
+          },
+        },
+        {
+          {
+            id = test_filepath .. "::TestExampleTestSuite::TestExample2",
+            name = "TestExample2",
+            path = test_filepath,
+            type = "test",
+          },
+        },
+      },
+      {
+        {
+          id = test_filepath .. "::ExampleTestSuite2",
+          name = "ExampleTestSuite2",
+          path = test_filepath,
+          type = "namespace",
+        },
+        {
+          {
+            id = test_filepath .. "::ExampleTestSuite2::TestExample",
+            name = "TestExample",
+            path = test_filepath,
+            type = "test",
+          },
+        },
+        {
+          {
+            id = test_filepath .. "::ExampleTestSuite2::TestExample2",
+            name = "TestExample2",
+            path = test_filepath,
+            type = "test",
+          },
+        },
+      },
+      {
+        {
+          id = test_filepath .. "::TestTrivial",
+          name = "TestTrivial",
+          path = test_filepath,
+          type = "test",
+        },
+      },
+      {
+        {
+          id = test_filepath .. "::TestOtherTestSuite",
+          name = "TestOtherTestSuite",
+          path = test_filepath,
+          type = "namespace",
+        },
+        {
+          {
+            id = test_filepath .. "::TestOtherTestSuite::TestOther",
+            name = "TestOther",
+            path = test_filepath,
+            type = "test",
+          },
+        },
+      },
+    }
+
+    -- Act
+    ---@type neotest.Tree
+    local tree =
+      nio.tests.with_async_context(adapter.discover_positions, test_filepath)
+
+    -- Assert
+    local result = tree:to_list()
+    local ignoreKeys = { range = true }
+    local expectedCopy, resultCopy =
+      compareIgnoringKeys(expected, result, ignoreKeys)
+    assert.are.same(vim.inspect(expectedCopy), vim.inspect(resultCopy))
+    assert.are.same(expectedCopy, resultCopy)
+  end)
+end)
