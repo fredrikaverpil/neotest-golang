6e071196ef9f1d9e053cc513e1b2b12dc97c950e
Jay Morelli <jcrmorelli@gmail.com>
2024-10-25 16:25:12 +0100

    feat: output colorizer

--- lua/neotest-golang/process.lua
+++ lua/neotest-golang/process.lua
@@ -302,6 +302,8 @@ function M.decorate_with_go_test_results(res, gotest_output)
         elseif line.Action == "fail" then
           test_data.status = "failed"
         elseif line.Action == "output" then
+          line.Output = M.colorizer(line.Output)
+
           test_data.gotest_data.output =
             vim.list_extend(test_data.gotest_data.output, { line.Output })
 
@@ -332,6 +334,26 @@ function M.decorate_with_go_test_results(res, gotest_output)
     end
   end
   return res
+end
+
+--- Colorize the test output based on the test result. 
+--- 
+--- It will colorize the test output line based on the test result (PASS - green, FAIL - red, SKIP - yellow).
+--- @param output string
+--- @return string
+function M.colorizer(output)
+  if not options.get().colorize_test_output == true or not output then
+    return output
+  end
+
+  if string.find(output, "FAIL") then
+    output = output:gsub("^", "[31m"):gsub("$", "[0m") -- colorizer red
+  elseif string.find(output, "PASS") then
+    output = output:gsub("^", "[32m"):gsub("$", "[0m") -- colorizer green
+  elseif string.find(output, "SKIP") then
+    output = output:gsub("^", "[33m"):gsub("$", "[0m") -- colorizer yellow
+  end
+  return output
 end
 
 --- Show warnings.
@@ -391,6 +413,7 @@ function M.to_neotest_result(res)
       errors = test_data.errors,
       output = test_output_path, -- NOTE: could be slow when running many tests?
     }
+    vim.notify(vim.inspect(neotest_result[pos_id]))
   end
 
   return neotest_result
