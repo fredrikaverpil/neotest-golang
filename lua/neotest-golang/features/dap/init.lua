78a81536d3cf009882770e2479d9801383147ee5
HeavyPunk <kirillkuchma8@gmail.com>
2024-11-20 01:36:50 +0500

    fix(dap_manual): removed preload of DAP strategy

--- lua/neotest-golang/features/dap/init.lua
+++ lua/neotest-golang/features/dap/init.lua
@@ -1,8 +1,6 @@
 --- DAP setup related functions.
 
 local options = require("neotest-golang.options")
-local dap_manual = require("neotest-golang.features.dap.dap_manual")
-local dap_go = require("neotest-golang.features.dap.dap_go")
 
 local M = {}
 
@@ -14,32 +12,33 @@ local function is_dap_manual_enabled()
   return dap_manual_enabled
 end
 
+local function get_dap_implementation()
+  local dap_impl
+  if is_dap_manual_enabled() then
+    dap_impl = require("neotest-golang.features.dap.dap_manual")
+  else
+    dap_impl = require("neotest-golang.features.dap.dap_go")
+  end
+  return dap_impl
+end
+
 ---@param cwd string
 function M.setup_debugging(cwd)
-  if is_dap_manual_enabled() then
-    dap_manual.setup_debugging(cwd)
-  else
-    dap_go.setup_debugging(cwd)
-  end
+  local dap_impl = get_dap_implementation()
+  dap_impl.setup_debugging(cwd)
 end
 
 --- @param test_path string
 --- @param test_name_regex string?
 --- @return table | nil
 function M.get_dap_config(test_path, test_name_regex)
-  if is_dap_manual_enabled() then
-    return dap_manual.get_dap_config(test_path, test_name_regex)
-  else
-    return dap_go.get_dap_config(test_path, test_name_regex)
-  end
+  local dap_impl = get_dap_implementation()
+  return dap_impl.get_dap_config(test_path, test_name_regex)
 end
 
 function M.assert_dap_prerequisites()
-  if is_dap_manual_enabled() then
-    dap_manual.assert_dap_prerequisites()
-  else
-    dap_go.assert_dap_prerequisites()
-  end
+  local dap_impl = get_dap_implementation()
+  dap_impl.assert_dap_prerequisites()
 end
 
 return M
