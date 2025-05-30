b678a598c5968f341197ac9243e4162a3c6a56ea
Étienne Lafarge <etienne.lafarge@gmail.com>
2025-05-30 18:21:20 +0200

    Add unit tests

---
+++ tests/unit/extra_args_spec.lua
@@ -0,0 +1,37 @@
+local _ = require("plenary")
+local extra_args = require("neotest-golang.extra_args")
+local lib = require("neotest-golang.lib")
+local options = require("neotest-golang.options")
+
+describe("Extra args", function()
+  it("Can't be nil even if set to nil", function()
+    extra_args.set(nil)
+    assert.are.same({}, extra_args.get())
+  end)
+
+  it("Returns the arg that were previously set", function()
+    extra_args.set({ "-foo", "-bar" })
+    assert.are.same({ "-foo", "-bar" }, extra_args.get())
+  end)
+
+  it("Overrides go_test_args when using the go test runner", function()
+    options.setup({ runner = "go", go_test_args = { "-foo", "-bar" } })
+    extra_args.set({ go_test_args = { "-baz", "-qux" } })
+
+    local command, _ = lib.cmd.test_command({})
+    assert.are.same({ "go", "test", "-json", "-baz", "-qux" }, command)
+  end)
+
+  it("Overrides go_test_args when using the gotestsum test runner", function()
+    options.setup({ runner = "gotestsum", go_test_args = { "-foo", "-bar" } })
+    extra_args.set({ go_test_args = { "-baz", "-qux" } })
+
+    local command, _ = lib.cmd.test_command({})
+    -- This parameter, the jsonfile path, contains a random string, let's get rid of it
+    table.remove(command, 2)
+    assert.are.same(
+      { "gotestsum", "--format=standard-verbose", "--", "-baz", "-qux" },
+      command
+    )
+  end)
+end)
