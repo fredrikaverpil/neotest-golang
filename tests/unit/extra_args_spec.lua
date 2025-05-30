5d504717c3ae878f6db41f20fcb24e5abfc98957
Étienne Lafarge <etienne.lafarge@gmail.com>
2025-05-30 18:34:20 +0200

    add defaulting test cases

--- tests/unit/extra_args_spec.lua
+++ tests/unit/extra_args_spec.lua
@@ -14,7 +14,7 @@ describe("Extra args", function()
     assert.are.same({ "-foo", "-bar" }, extra_args.get())
   end)
 
-  it("Overrides go_test_args when using the go test runner", function()
+  it("Overrides go_test_args in go test command", function()
     options.setup({ runner = "go", go_test_args = { "-foo", "-bar" } })
     extra_args.set({ go_test_args = { "-baz", "-qux" } })
 
@@ -22,7 +22,7 @@ describe("Extra args", function()
     assert.are.same({ "go", "test", "-json", "-baz", "-qux" }, command)
   end)
 
-  it("Overrides go_test_args when using the gotestsum test runner", function()
+  it("Overrides go_test_args in gotestsum command", function()
     options.setup({ runner = "gotestsum", go_test_args = { "-foo", "-bar" } })
     extra_args.set({ go_test_args = { "-baz", "-qux" } })
 
@@ -31,6 +31,27 @@ describe("Extra args", function()
     table.remove(command, 2)
     assert.are.same(
       { "gotestsum", "--format=standard-verbose", "--", "-baz", "-qux" },
+      command
+    )
+  end)
+
+  it("Defaults to go_test_args in go test", function()
+    options.setup({ runner = "go", go_test_args = { "-foo", "-bar" } })
+    extra_args.set({})
+
+    local command, _ = lib.cmd.test_command({})
+    assert.are.same({ "go", "test", "-json", "-foo", "-bar" }, command)
+  end)
+
+  it("Defaults to go_test_args in gotestsum", function()
+    options.setup({ runner = "gotestsum", go_test_args = { "-foo", "-bar" } })
+    extra_args.set({})
+
+    local command, _ = lib.cmd.test_command({})
+    -- This parameter, the jsonfile path, contains a random string, let's get rid of it
+    table.remove(command, 2)
+    assert.are.same(
+      { "gotestsum", "--format=standard-verbose", "--", "-foo", "-bar" },
       command
     )
   end)
