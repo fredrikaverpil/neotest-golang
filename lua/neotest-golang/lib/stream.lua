fc9d8c4c7d838f0235c77a608aaf0133443c9595
Fredrik Averpil <fredrik.averpil@gmail.com>
2025-08-10 01:31:21 +0200

    feat: parse test event

--- lua/neotest-golang/lib/stream.lua
+++ lua/neotest-golang/lib/stream.lua
@@ -1,3 +1,4 @@
+local convert = require("neotest-golang.lib.convert")
 local json = require("neotest-golang.lib.json")
 local logger = require("neotest-golang.logging")
 local options = require("neotest-golang.options")
@@ -6,12 +7,21 @@ local neotest_lib = require("neotest.lib")
 
 local M = {}
 
+--- Convert to internal and unique test id for lookup.
+local function to_test_id(package_name, test_name)
+  return package_name .. "::" .. test_name
+end
+
 --- Contstructor for new stream.
+--- @param golist_data table Golist data containing package information
 ---@param json_filepath string|nil Path to the JSON output file
 ---@return function, function
-function M.new(json_filepath)
-  local stream_data = function() end
-  local stop_stream = function() end
+function M.new(tree, golist_data, json_filepath)
+  -- vim.notify(vim.inspect("New stream!"))
+
+  M.accumulated_test_data = {} -- reset
+  local stream_data = function() end -- no-op
+  local stop_stream = function() end -- no-op
   if options.get().runner == "gotestsum" then
     if json_filepath ~= nil then
       neotest_lib.files.write(json_filepath, "") -- ensure the file exists
@@ -24,48 +34,83 @@ function M.new(json_filepath)
   --- Stream function.
   ---@param data function A function that returns a table of strings, each representing a line of JSON output.
   local function stream(data)
+    local tree = tree
+    local golist_data = golist_data
+    local json_lines = {}
+    local accum = {}
+
+    ---@type table<string, neotest.Result>
+    local results = {}
+
     return function()
-      local json_lines = {}
-      local results = {}
+      local lines = {}
       if options.get().runner == "go" then
-        local lines = data() -- capture from stdout
-        for _, line in ipairs(lines) do
-          json_lines =
-            vim.list_extend(json_lines, json.decode_from_string(line))
-        end
+        lines = data() -- capture from stdout
       elseif options.get().runner == "gotestsum" then
-        local lines = stream_data() -- capture from stream
-        for _, line in ipairs(lines) do
-          json_lines = json.decode_from_string(line)
+        lines = stream_data() -- capture from stream
+      end
+
+      for _, line in ipairs(lines) do
+        json_lines = vim.list_extend(json_lines, json.decode_from_string(line))
+        for _, json_line in ipairs(json_lines) do
+          accum = M.process_event(tree, golist_data, accum, json_line)
         end
       end
 
-      for _, json_line in ipairs(json_lines) do
-        -- started test detected
-        if json_line.Action == "start" and json_line.Test ~= nil then
-          vim.notify(vim.inspect(json_line))
-          -- TODO: store ongoing test in lookup table
-        end
-
-        -- running test detected
-        if json_line.Action == "output" and json_line.Test ~= nil then
-          vim.notify(vim.inspect(json_line))
-          -- TODO: store ongoing test in lookup table
-        end
-
-        -- passed test
-        if json_line.Action == "pass" and json_line.Test ~= nil then
-          vim.notify("PASS: " .. vim.inspect(json_line))
-          -- TODO: map test/package to position id, add to results when all started tests have a status
+      for _, test_data in pairs(accum) do
+        if test_data.status == "passed" then
+          results[test_data.position_id] = {
+            status = test_data.status, -- passed/failed/skipped
+            output = test_data.output,
+            -- TODO: add short
+            -- TODO: add errors
+          }
         end
       end
 
-      -- TODO: return results only if all ongoign tests have a status
+      -- TODO: only return a result when a test has a status (pass/fail/skip), otherwise return {}
       return results
     end
   end
 
   return stream, stop_stream
+end
+
+--- Process a single event from the test output.
+--- @param accum table Accumulated test data.
+--- @param e table The event data.
+function M.process_event(tree, golist_data, accum, e)
+  -- TODO: do we want to do something with 'start' status?
+
+  -- Indicate test started/running.
+  if e.Action == "run" and e.Test ~= nil then
+    local id = to_test_id(e.Package, e.Test)
+    accum[id] = { status = "running", output = "" }
+    if e.Output ~= nil then
+      accum[id].output = e.Output
+    end
+  end
+
+  -- Record output.
+  if e.Action == "output" and e.Test ~= nil and e.Output ~= nil then
+    local id = to_test_id(e.Package, e.Test)
+    accum[id].output = accum[id].output .. "\n" .. e.Output
+  end
+
+  -- Passed test.
+  if e.Action == "pass" and e.Test ~= nil then
+    local id = to_test_id(e.Package, e.Test)
+    accum[id].status = "passed"
+    if e.Output ~= nil then
+      accum[id].output = e.Output
+    end
+
+    local pattern =
+      convert.to_position_id_pattern(golist_data, e.Package, e.Test)
+    accum[id].position_id = convert.to_position_id(tree, pattern)
+  end
+
+  return accum
 end
 
 return M
