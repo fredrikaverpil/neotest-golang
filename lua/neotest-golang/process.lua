a426520860620421e61b6341f2ca431f95d7d125
Fredrik Averpil <fredrik.averpil@gmail.com>
2025-09-04 06:45:04 +0200

    fix: cached results imports

--- lua/neotest-golang/process.lua
+++ lua/neotest-golang/process.lua
@@ -74,8 +74,8 @@ function M.test_results(spec, result, tree)
 
   -- Re-register cached results.
   ---@type table<string, neotest.Result>
-  local results = require("neotest-golang.lib.stream").cached_results -- TODO: fix circular dependency
-  require("neotest-golang.lib.stream").cached_results = {} -- clear cache
+  local results = lib.stream.cached_results
+  lib.stream.cached_results = {} -- clear cache
 
   results[pos.id] = M.node_results(results[pos.id], result, gotest_output) -- register root node result
 
