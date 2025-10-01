583c0a03d42181a7be3f613c8d124ebca2eae692
Fredrik Averpil <fredrik.averpil@gmail.com>
2025-10-01 08:26:34 +0200

    chore(query): remove unused

--- lua/neotest-golang/lib/query_loader.lua
+++ lua/neotest-golang/lib/query_loader.lua
@@ -21,15 +21,10 @@ function M.load_query(query_path)
   return content
 end
 
---- Load multiple queries and concatenate them
---- @param query_paths table List of relative paths to .scm files
---- @return string The concatenated query content
-function M.load_queries(query_paths)
-  local queries = {}
-  for _, path in ipairs(query_paths) do
-    table.insert(queries, M.load_query(path))
-  end
-  return table.concat(queries, "\n")
+--- Clear the query cache (useful for testing)
+--- @return nil
+function M.clear_cache()
+  query_cache = {}
 end
 
 return M
