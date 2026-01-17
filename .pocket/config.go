1af9cf71dc994483ab6c827b9e18768c3870ced8
Fredrik Averpil <fredrik.averpil@gmail.com>
2026-01-17 22:12:59 +0100

    feat: new workflow

--- .pocket/config.go
+++ .pocket/config.go
@@ -12,26 +12,39 @@ import (
 	"github.com/fredrikaverpil/pocket/tools/tsqueryls"
 )
 
+// autoRun defines the tasks that run on ./pok with no arguments.
+var autoRun = pocket.Serial(
+	// Lua formatting
+	pocket.RunIn(lua.Tasks(), pocket.Detect(lua.Detect())),
+
+	// Python workflow (format, lint, typecheck, test)
+	pocket.RunIn(python.Tasks(), pocket.Detect(python.Detect())),
+
+	// Go workflow for test code
+	pocket.RunIn(golang.Tasks(),
+		pocket.Detect(golang.Detect()),
+		pocket.Skip(golang.Test, "tests/go", "tests/features"),
+	),
+
+	// Tree-sitter query formatting
+	QueryFormat,
+
+	// Plenary tests
+	PlenaryTest,
+)
+
+// matrixConfig configures GitHub Actions matrix generation.
+var matrixConfig = github.MatrixConfig{
+	DefaultPlatforms: []string{"ubuntu-latest"},
+	ExcludeTasks:     []string{"github-workflows", "plenary-test"},
+}
+
 // Config is the pocket configuration for this project.
 var Config = pocket.Config{
-	AutoRun: pocket.Serial(
-		// Lua formatting
-		pocket.Paths(lua.Tasks()).DetectBy(lua.Detect()),
-
-		// Python workflow (format, lint, typecheck, test)
-		pocket.Paths(python.Tasks()).DetectBy(python.Detect()),
-
-		// Go workflow for test code
-		pocket.Paths(golang.Tasks()).SkipTask(golang.Test, "testing/go", "testing/features").DetectBy(golang.Detect()),
-
-		// Tree-sitter query formatting
-		QueryFormat,
-
-		// Plenary tests
-		PlenaryTest,
-	),
+	AutoRun: autoRun,
 	ManualRun: []pocket.Runnable{
 		github.Workflows,
+		github.MatrixTask(autoRun, matrixConfig),
 	},
 	Shim: &pocket.ShimConfig{
 		Posix: true,
@@ -39,23 +52,25 @@ var Config = pocket.Config{
 }
 
 // QueryFormat formats tree-sitter query files using ts_query_ls.
-var QueryFormat = pocket.Func("query-format", "format tree-sitter query files", pocket.Serial(
+var QueryFormat = pocket.Task("query-format", "format tree-sitter query files", pocket.Serial(
 	tsqueryls.Install,
-	queryFormat,
+	queryFormat(),
 ))
 
-func queryFormat(ctx context.Context) error {
-	queryDirs := []string{
-		"lua/neotest-golang/queries",
-		"lua/neotest-golang/features/testify/queries",
-	}
-	for _, dir := range queryDirs {
-		absDir := pocket.FromGitRoot(dir)
-		if err := pocket.Exec(ctx, tsqueryls.Name, "format", absDir); err != nil {
-			return err
+func queryFormat() pocket.Runnable {
+	return pocket.Do(func(ctx context.Context) error {
+		queryDirs := []string{
+			"lua/neotest-golang/queries",
+			"lua/neotest-golang/features/testify/queries",
 		}
-	}
-	return nil
+		for _, dir := range queryDirs {
+			absDir := pocket.FromGitRoot(dir)
+			if err := pocket.Exec(ctx, tsqueryls.Name, "format", absDir); err != nil {
+				return err
+			}
+		}
+		return nil
+	})
 }
 
 // PlenaryTestOptions configures the plenary-test task.
@@ -64,34 +79,36 @@ type PlenaryTestOptions struct {
 }
 
 // PlenaryTest runs tests with Neovim and plenary.
-var PlenaryTest = pocket.Func("plenary-test", "run tests with Neovim and plenary", pocket.Serial(
+var PlenaryTest = pocket.Task("plenary-test", "run tests with Neovim and plenary", pocket.Serial(
 	nvim.Install,
-	plenaryTest,
-)).With(PlenaryTestOptions{})
+	plenaryTest(),
+), pocket.Opts(PlenaryTestOptions{}))
 
-func plenaryTest(ctx context.Context) error {
-	opts := pocket.Options[PlenaryTestOptions](ctx)
+func plenaryTest() pocket.Runnable {
+	return pocket.Do(func(ctx context.Context) error {
+		opts := pocket.Options[PlenaryTestOptions](ctx)
 
-	if opts.File != "" {
-		// Run single test file
-		return pocket.Exec(
-			ctx,
-			nvim.Name,
-			"--headless",
-			"--noplugin",
-			"-i",
-			"NONE",
-			"-u",
-			"spec/bootstrap.lua",
-			"-c",
-			"lua require('plenary.test_harness').test_directory_command('"+opts.File+" { minimal_init = \"spec/minimal_init.lua\", timeout = 500000 }')",
+		if opts.File != "" {
+			// Run single test file
+			return pocket.Exec(
+				ctx,
+				nvim.Name,
+				"--headless",
+				"--noplugin",
+				"-i",
+				"NONE",
+				"-u",
+				"spec/bootstrap.lua",
+				"-c",
+				"lua require('plenary.test_harness').test_directory_command('"+opts.File+" { minimal_init = \"spec/minimal_init.lua\", timeout = 500000 }')",
+			)
+		}
+
+		// Run all tests
+		return pocket.Exec(ctx, nvim.Name,
+			"--headless", "--noplugin", "-i", "NONE",
+			"-u", "spec/bootstrap.lua",
+			"-c", "PlenaryBustedDirectory spec/ { minimal_init = 'spec/minimal_init.lua', timeout = 500000 }",
 		)
-	}
-
-	// Run all tests
-	return pocket.Exec(ctx, nvim.Name,
-		"--headless", "--noplugin", "-i", "NONE",
-		"-u", "spec/bootstrap.lua",
-		"-c", "PlenaryBustedDirectory spec/ { minimal_init = 'spec/minimal_init.lua', timeout = 500000 }",
-	)
+	})
 }
--- .pocket/go.mod
+++ .pocket/go.mod
@@ -2,7 +2,7 @@ module pocket
 
 go 1.25.5
 
-require github.com/fredrikaverpil/pocket v0.0.0-20260116162149-030f753a18b6
+require github.com/fredrikaverpil/pocket v0.0.0-20260117184422-3cdce55acd33
 
 require (
 	golang.org/x/sync v0.19.0 // indirect
